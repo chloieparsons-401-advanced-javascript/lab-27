4159dc35e3e44d0efff5293abf8551f7bbf1fd89
Chloie Parsons <chloieparsons@gmail.com>
2019-07-02 19:00:02 -0700

    init commit

---
+++ src/footer/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "./footer.scss";
+
+class Footer extends Component {
+  render() {
+    return <footer>&copy; 2019 Code Fellows</footer>;
+  }
+}
+
+export default Footer;
